c4a8a94b5f7e5c4fafd572fc570382bb7fecb19d
Alexandre Detiste <alexandre.detiste@gmail.com>
2026-04-10 19:45:59 +0200

    OO

--- repos.py
+++ repos.py
@@ -34,34 +34,56 @@ def tick(flag: bool) -> str:
     else:
         return Color('{autored}✗{/autored}')
 
+
+class Binary:
+    '''some binary artefact'''
+    filename: str
+    arch: str
+
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.arch = filename.split('_')[2].split('.')[0]
+
+
+class Package:
+    '''one package might have 1 or more binaries'''
+    name: str
+    binaries = set()
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.binaries = set()
+
+    def add(self, binary: Binary) -> None:
+        self.binaries.add(binary)
+
 def get_packages() -> dict[str, set[str]]:
     '''get one or two .deb per package'''
-    packages: dict[str, set[str]] = dict()
+    packages: dict[str, Package] = dict()
 
     for p in sorted(glob.glob('*.deb')):
         basename = os.path.basename(p)
         parts = basename.split('_')
         name = parts[0]
         if name not in packages:
-            packages[name] = set()
-        arch = parts[2].split('.')[0]
-        packages[name].add(arch)
+            packages[name] = Package(name)
+        binary = Binary(p)
+        packages[name].add(binary)
     return packages
 
 table_data = [['package', 'arch', 'buster', 'trixie']]
 
-for package, archs in get_packages().items():
-    if len(archs) == 2:
-        table_data.append([package, 'i386', tick(True), tick(False)])
-        table_data.append([package, 'amd64', tick(False), tick(True)])
+for package in get_packages().values():
+    if len(package.binaries) == 2:
+        table_data.append([package.name, 'i386', tick(True), tick(False)])
+        table_data.append([package.name, 'amd64', tick(False), tick(True)])
     else:
-        arch = archs.pop()
-        filename = glob.glob('%s*%s.deb' % (package, arch))[0]
-        depends = subprocess.check_output(['dpkg-deb', '--field', filename, 'Depends'],
+        binary = package.binaries.pop()
+        depends = subprocess.check_output(['dpkg-deb', '--field', binary.filename, 'Depends'],
                                            text=True).strip('\n ')
 
         buster, trixie = relevant(depends)
-        table_data.append([package, arch, tick(buster), tick(trixie)])
+        table_data.append([package.name, binary.arch, tick(buster), tick(trixie)])
 
 render = SingleTable(table_data, 'Sorting Hat')
 print(render.table)
