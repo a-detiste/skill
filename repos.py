af3c409b0471fc73af543a4c08b299f3b7418d0d
Alexandre Detiste <alexandre.detiste@gmail.com>
2026-04-10 19:25:32 +0200

    correct

--- repos.py
+++ repos.py
@@ -35,21 +35,35 @@ def tick(flag: bool) -> str:
     else:
         return Color('{autored}✗{/autored}')
 
+def get_packages() -> dict[str, set[str]]:
+    packages: dict[str, set[str]] = dict()
+
+    for p in sorted(glob.glob('*.deb')):
+        basename = os.path.basename(p)
+        parts = basename.split('_')
+        name = parts[0]
+        if name not in packages:
+            packages[name] = set()
+        arch = parts[2].split('.')[0]
+        packages[name].add(arch)
+    return packages
+
 table_data = [['package', 'arch', 'buster', 'trixie']]
 
-for p in sorted(glob.glob('*.deb')):
-    basename = os.path.basename(p)
-    parts = basename.split('_')
-    name = parts[0]
+for package, archs in get_packages().items():
+    if len(archs) == 2:
+        table_data.append([package, 'i386', tick(True), tick(False)])
+        table_data.append([package, 'amd64', tick(False), tick(True)])
+    else:
+        arch = archs.pop()
+        depends = subprocess.check_output(['dpkg-deb',
+                                           '--field',
+                                           glob.glob('%s*%s.deb' % (package, arch))[0],
+                                           'Depends'],
+                                           text=True).strip('\n ')
 
-    arch = parts[2].split('.')[0]
-
-    depends = subprocess.check_output(['dpkg-deb', '--field', p, 'Depends'],
-                                      text=True).strip('\n ')
-
-
-    buster, trixie = relevant(depends)
-    table_data.append([name, arch, tick(buster), tick(trixie)])
+        buster, trixie = relevant(depends)
+        table_data.append([package, arch, tick(buster), tick(trixie)])
 
 render = SingleTable(table_data, 'Sorting Hat')
 print(render.table)
